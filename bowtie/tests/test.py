1c8b664efb12ab25e23fb6ca0a662af3d7c19665
Jan Gieseler <jan.gieseler@utu.fi>
2024-12-16 16:45:45 +0200

    Fix baseline image path in test instructions

--- bowtie/tests/test.py
+++ bowtie/tests/test.py
@@ -13,7 +13,7 @@ Install dependencies for tests:
 pip install flake8 pytest pytest-doctestplus pytest-cov pytest-mpl
 
 To create/update the baseline images, run the following command from the base package dir:
-pytest --mpl-generate-path=tests/baseline bowtie/tests/test.py
+pytest --mpl-generate-path=bowtie/tests/baseline bowtie/tests/test.py
 
 To run the tests locally, go to the base directory of the repository and run:
 pytest -rP --mpl --mpl-baseline-path=baseline --mpl-baseline-relative --mpl-generate-summary=html --cov=bowtie bowtie/tests/test.py
